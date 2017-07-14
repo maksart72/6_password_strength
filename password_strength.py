3804e805a0134cbcb4ca70e872daebfee29c5296
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-14 15:52:31 +0500

    Formated with pep8

--- password_strength.py
+++ password_strength.py
@@ -1,17 +1,16 @@
 import re
-    
+
+
 def get_password_strength(password):
-
     strength = 0
-    is_small = 1 
-    is_caps = 1 
-    is_number = 1 
-    is_symbol = 1 
-    is_length = 0 
-
+    is_small = 1
+    is_caps = 1
+    is_number = 1
+    is_symbol = 1
+    is_length = 0
     if re.search('[a-zа-я]', password) is None:
         is_small = 0
-    
+
     if re.search('[A-ZА-Я]', password) is None:
         is_caps = 0
 
@@ -24,17 +23,19 @@ def get_password_strength(password):
     pass_length = len(password) // 3
     if pass_length > 5:
         is_length = 6
-    else: 
+    else:
         is_length = pass_length
 
-    strength = is_caps+is_length+is_small+is_number+is_symbol
-    return(str(strength))
+    strength = is_caps + is_length + is_small + is_number + is_symbol
+    return str(strength)
+
 
 if __name__ == '__main__':
     print('')
-    password = input('Enter your password:')
-    if re.search(' ',password) is None:
-       print('The strength of your password is: '+ get_password_strength(password))
-       print('P.S. 1 – Password is very weak..., 10 – Password is very strong! ')
+    PASSWORD_IS = input('Enter your password:')
+    if re.search(' ', PASSWORD_IS) is None:
+        print('The strength of your password is: ' +
+              get_password_strength(PASSWORD_IS))
+        print('1 – Password is very weak..., 10 – Password is very strong! ')
     else:
-       raise ValueError('Error! Space is permitted')
+        raise ValueError('Error! Space is permitted')

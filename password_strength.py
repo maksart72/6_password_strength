cc64f901ff8e359f6c4576121cded22182dd2d9f
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-17 15:45:40 +0500

    Made password invisible, remove some variables, fix space case.

--- password_strength.py
+++ password_strength.py
@@ -1,41 +1,26 @@
 import re
+import getpass
 
 
 def get_password_strength(password):
     strength = 0
-    is_small = 1
-    is_caps = 1
-    is_number = 1
-    is_symbol = 1
-    is_length = 0
-    if re.search('[a-zа-я]', password) is None:
-        is_small = 0
-
-    if re.search('[A-ZА-Я]', password) is None:
-        is_caps = 0
-
-    if re.search('[0-9]', password) is None:
-        is_number = 0
-
-    if re.search('[^A-zА-я0-9]', password) is None:
-        is_symbol = 0
-
+    if re.search('[a-zа-я]', password) is not None:
+        strength = strength + 1
+    if re.search('[A-ZА-Я]', password) is not None:
+        strength = strength + 1
+    if re.search('[0-9]', password) is not None:
+        strength = strength + 1
+    if re.search('[^A-zА-я0-9]', password) is not None:
+        strength = strength + 1
     pass_length = len(password) // 3
     if pass_length > 5:
-        is_length = 6
+        strength = strength + 6
     else:
-        is_length = pass_length
-
-    strength = is_caps + is_length + is_small + is_number + is_symbol
+        strength = strength + pass_length
     return str(strength)
-
 
 if __name__ == '__main__':
     print('')
-    PASSWORD_IS = input('Enter your password:')
-    if re.search(' ', PASSWORD_IS) is None:
-        print('The strength of your password is: ' +
-              get_password_strength(PASSWORD_IS))
-        print('1 – Password is very weak..., 10 – Password is very strong! ')
-    else:
-        raise ValueError('Error! Space is permitted')
+    userpassword = getpass.getpass('Enter your password:')
+    print('The strength of your password is: ' + get_password_strength(userpassword))
+    print('1 – Password is very weak..., 10 – Password is very strong! ')
